be5e2550e432f2cda3e2134d4c698f9c89aee623
Ryan Ziegler <rzig408@gmail.com>
2020-03-22 13:03:38 -0400

    added login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,10 @@ import { Router, Link, Match } from '@reach/router';
 import Sensors from './pages/Sensors';
 import firebase from "firebase";
 import { firebaseConfig } from './config';
+import Login from './pages/Login';
+import { User } from './types/User';
 require("firebase/firestore");
+require("firebase/auth");
 
 
 type NavLinkProps = {
@@ -43,8 +46,30 @@ function NavLink({name, to, icon}: NavLinkProps) {
 }
 
 function App() {
-  return (
-    
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [email, setEmail] = useState<string|undefined>();
+  const [password, setPassword] = useState<string|undefined>();
+
+  const [incorrect, setIncorrect] = useState<boolean>(false);
+
+  const handleSubmit = () => {
+    firebase.auth().signInWithEmailAndPassword(email ?? "", password ?? "").then((user) => {
+      const uid = user.user?.uid;
+      firebase.firestore().collection("administrators").doc(uid).get().then(document => {
+        if(document.exists) {
+          setIsAuthenticated(true);
+        } else {
+          setIncorrect(true);
+        }
+      }).catch(() => {
+      })
+    }).catch(err => {
+      setIncorrect(true);
+    })
+  }
+
+  if(isAuthenticated) {
+    return (
       <div className="container grid grid-cols-8 h-screen bg-gray-200 w-screen max-w-none overflow-x-hidden w-screen">
         <aside className="bg-white col-span-1 p-5 flex flex-col">
           <h1 className="text-2xl border-b pb-3 border-gray-400 h-12">Dashboard</h1>
@@ -63,7 +88,47 @@ function App() {
           </Router>
         </div>
       </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container grid grid-cols-8 h-screen bg-gray-200 w-screen overflow-x-hidden max-w-none">
+        <div className="col-span-2 col-start-4 flex items-center justify-center h-full">
+          <div className=" w-full bg-white rounded shadow px-6 py-6">
+            <h1 className="text-2xl text-center pb-6">Login</h1>
+            <form onSubmit={(e) => {handleSubmit(); e.preventDefault()}}>
+              <input 
+                placeholder="Email"
+                autoFocus 
+                className="py-3 border w-full pl-3 rounded border-gray-400 mb-4 focus:border-gray-600 transition duration-100" 
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                placeholder="Password"
+                className="py-3 border w-full pl-3 rounded border-gray-400 focus:border-gray-600 transition duration-100"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {incorrect && 
+                <p className="text-center pt-8">Incorrect username or password.</p>
+              }
+              <button
+                className="bg-blue-600 w-full py-3 mt-8 text-white rounded"
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+    path: string
+}
+function Login({}: Props) {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
+
+export default Login;
